6a80e5a66a5fdc6093a7947a02d1d774c1c42271
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2023-01-20 15:59:32 -0600

    - change to subject format

--- tests/student/student.rb
+++ tests/student/student.rb
@@ -1,13 +1,20 @@
 class Student
   attr_reader :first_name, :last_name
+  @@total_count = 0
 
   def initialize(first, last)
     @first_name = first
     @last_name = last
+    @@total_count += 1
   end
 
   def student_full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.total_count
+    @@total_count
+  end
+  
 
 end
--- tests/student/student_spec.rb
+++ tests/student/student_spec.rb
@@ -2,21 +2,22 @@ require_relative 'student'
 
 RSpec.describe Student do
   describe "instance methods" do
-    before do
-      @student = Student.new('John','Doe')
-    end
 
     subject { Studen.new('John','Doe') }
-    it 'should respond respond to #first_name' do
-      expect(@student).to respond_to(:first_name)
-    end
 
-    it 'should respond respond to #last_name' do
-      expect(@student).to respond_to(:last_name)
-    end
+    it { respond_to(:first_name) }
 
-    it 'should respond respond to #student_full_name' do
-      expect(@student).to respond_to(:student_full_name)
+    it { respond_to(:last_name) }
+
+    it { respond_to(:student_full_name) }
+
+    describe 'total number of students created' do
+      it 'should have students in total' do
+        Student.new('John','Doe')
+        Student.new('Name','Doe')
+
+        expect(Student.total_count).to eq(2)
+      end
     end
   end
 end
